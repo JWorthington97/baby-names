426637a78826e6734af41faeb47c7ba06f4e8a11
JWorthington97 <70533356+JWorthington97@users.noreply.github.com>
2021-07-03 12:34:59 +0100

    Fix search case sensitivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
-import BabyNames from './components/BabyNames';
+import "./App.css";
+import BabyNames from "./components/BabyNames";
 function App() {
   return (
     <div>
-    <BabyNames/>
+      <BabyNames />
     </div>
-  )
+  );
 }
 
 export default App;
